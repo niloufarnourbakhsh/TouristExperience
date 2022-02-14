e64188b563dc90f7f998a119082f17f9274993ec
niloofar.noorbakhsh <niloofar.noorbakhsh@yahoo.com>
2022-02-14 23:59:52 +0330

    validate the fields and then  create a message throw a session and get back to the main page

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class MessagesController extends Controller
 {
     public function create()
     {
-        return view('user.cantact');
+
+        return view('user.contact');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email',
+            'body' => 'required',
+        ],[
+            'name.required'=> 'لطفا نام خود را وارد کنید',
+            'email.required'=> 'لطفا ایمیل خود را وارد کنید',
+            'body.required'=> 'لطفا پیام خود را وارد کنید'
+        ]);
+
+        Message::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'body'=>$request->body
+        ]);
+
+        session()->flash('messages','پیام شما ثبت شد');
+        return redirect()->back();
     }
 }
