2afd9e0394d8c41202fd1136f3461bf57b331837
niloofar.noorbakhsh <niloofar.noorbakhsh@yahoo.com>
2022-02-14 23:58:24 +0330

    add error messages to the blad. if a field is empty, show an error message

--- resources/views/user/contact.blade.php
+++ resources/views/user/contact.blade.php
@@ -9,20 +9,28 @@
         <h4 class="text-center"> تماس با ما</h4>
         <div class="hr m-auto bg-magenta" ></div>
         <div class="container mt-4 mb-3 text-right">
-            <form action="{{route('contact-us')}}" method="POST">
+            <form action="{{url('/contact/store')}}" method="POST">
                 @csrf
                 <div class="form-group">
                     <label for="name"> نام شما:</label>
                     <input type="text" name="name" class="form-control my-input-style" id="name">
+                    @if($errors->first('name'))
+                        <p class="text-danger text-bold">{{$errors->first('name')}}</p>
+                    @endif
                 </div>
                 <div class="form-group">
                     <label for="email"> ایمیل شما: </label>
-                    <input type="text" name="email" id="email" class="form-control my-input-style">
-
+                    <input type="email" name="email" id="email" class="form-control my-input-style">
+                    @if($errors->first('email'))
+                        <p class="text-danger text-bold">{{$errors->first('email')}}</p>
+                    @endif
                 </div>
                 <div class="form-group">
                     <label>پیام شما</label>
-                    <textarea class="my-input-style form-control"></textarea>
+                    <textarea class="my-input-style form-control" name="body"></textarea>
+                    @if($errors->first('body'))
+                        <p class="text-danger text-bold">{{$errors->first('body')}}</p>
+                    @endif
                 </div>
 
                 <button class="btn btn-block btn-colorful text-white" type="submit">ارسال پیام</button>
