6ad19bb61ba7304b42068bf65ec50709b780fff7
niloofar.noorbakhsh <niloofar.noorbakhsh@yahoo.com>
2022-02-14 23:37:26 +0330

    added messages fields and migrate

--- database/migrations/2022_02_14_185924_create_messages_table.php
+++ database/migrations/2022_02_14_185924_create_messages_table.php
@@ -15,6 +15,9 @@ class CreateMessagesTable extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->text('body');
             $table->timestamps();
         });
     }
