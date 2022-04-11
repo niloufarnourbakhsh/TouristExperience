34329aee5f8fd7369f59f7203b013f16e85cc2a1
niloofar.noorbakhsh <niloofar.noorbakhsh@yahoo.com>
2022-04-12 00:20:20 +0430

    create method changed

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,35 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\MessageRequest;
+use App\Http\Requests\CommentsRequest;
 use App\Models\Comment;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 
 class CommentController extends Controller
 {
-    //
 
     public function index()
     {
-        $comments=Comment::with('post')->paginate(8);
-        return view('Admin.message')->with('comments',$comments);
+        $comments = Comment::with('post')->paginate(8);
+        return view('Admin.comments')->with('comments', $comments);
     }
 
-
-    public function create(MessageRequest $request)
+    public function create(CommentsRequest $request)
     {
-
-        Auth::user()->comments()->create(['post_id'=>$request->post_id,'body'=>$request->body]);
+        Auth::user()->comments()->create($request->validated());
         return redirect()->back();
     }
 
-    public function delete($id){
-
+    public function delete($id)
+    {
         Comment::find($id)->delete();
-        Session::flash('delete_message','پیام موورد نظر حذف شد');
+        Session::flash('delete_message', 'پیام موورد نظر حذف شد');
         return redirect()->back();
     }
-
+    
 }
