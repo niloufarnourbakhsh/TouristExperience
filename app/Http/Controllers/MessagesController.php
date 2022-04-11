e4ed30f5f129f47a36db0644678e8706d82ce4af
niloofar.noorbakhsh <niloofar.noorbakhsh@yahoo.com>
2022-04-11 22:46:07 +0430

    delete and index method added to this controller

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -8,17 +8,27 @@ use Illuminate\Http\Request;
 
 class MessagesController extends Controller
 {
+    public function index()
+    {
+        $messages = Message::query()->paginate();
+        return view('Admin.messages')->with('messages', $messages);
+    }
+
     public function create()
     {
-
         return view('user.contact');
     }
 
     public function store(MessageRequest $request)
     {
-
         Message::create($request->validated());
-        session()->flash('messages','پیام شما ثبت شد');
+        session()->flash('messages', 'پیام شما ثبت شد');
+        return redirect()->back();
+    }
+
+    public function delete($id)
+    {
+        Message::query()->find($id)->delete();
         return redirect()->back();
     }
 }
